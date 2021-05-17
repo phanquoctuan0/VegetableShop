6d7d5149197315df21c5c784f2f24e3617d30130
tuanphan2k <phanquoctuan00@gmail.com>
2021-05-18 00:57:42 +0700

    edit admin page management user

--- src/pages/admin/UserMana/index.jsx
+++ src/pages/admin/UserMana/index.jsx
@@ -1,13 +1,11 @@
-import { Input } from 'antd';
-import { PlusSquareOutlined } from '@ant-design/icons';
-import { Modal, Button } from 'antd';
+import { Table, Modal, Space, Radio, Input } from 'antd';
+import { EditOutlined, UserDeleteOutlined, PlusSquareOutlined } from '@ant-design/icons';
 
 import { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 
 import { getUserListAction, deleteUserAction } from '../../../redux/actions';
 
-import UserItem from './components/UserItem';
 import Register from '../../Register';
 
 function AdminUserPage({
@@ -15,19 +13,46 @@ function AdminUserPage({
   userList,
   deleteUser,
 }) {
-
   useEffect(() => {
     getUserList();
   }, []);
+  const userInfo = JSON.parse(localStorage.getItem("userInfo"));
 
   const { Search } = Input;
   const onSearch = value => console.log(value);
-
-  const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+  const { Column } = Table;
 
   const [isModalVisible, setIsModalVisible] = useState(false);
-
   const [isModalVisible2, setIsModalVisible2] = useState(false);
+
+  const [valueRadio, setValueRadio] = useState(1);
+  const [isIdEdit, setIsIdEdit] = useState(null);
+
+  const onChange = e => {
+    console.log(e.target.value);
+    setValueRadio(e.target.value);
+  };
+
+  console.log("🚀 ~ file: index.jsx ~ line 36 ~ userList.data.forEach ~ userList", userList.data)
+  function handleEditUser(id) {
+    userList.data.forEach((item) => {
+      if (id === item.id) {
+        const user = {
+          id: item.id,
+          email: item.email,
+          password: item.password,
+          name: item.name,
+          phone: item.phone,
+          role: valueRadio
+        }
+        deleteUser({
+          id: id,
+          user: user
+        })
+        setIsModalVisible(false)
+      }
+    })
+  }
 
   function handleDeleteUser(id) {
     userList.data.forEach((item) => {
@@ -47,74 +72,81 @@ function AdminUserPage({
       }
     })
   }
-
-  function handleEditUser(id, values) {
-    console.log("🚀 ~ file: index.jsx ~ line 42 ~ handleEditUser ~ values", values)
-    const user = {
-      id: id,
-      email: values.email,
-      password: values.item,
-      name: values.name,
-      phone: values.phone,
-      role: values.role
-    }
-    deleteUser({
-      id: id,
-      user: user
-    })
-  }
-
-  function renderUserList() {
-    if (userList.load) return <p>Loading...</p>;
-    return userList.data.map((userItem, productIndex) => {
-      if (userItem.role !== 'banned' && userItem.id !== userInfo.id) {
-        return (
-          <UserItem
-            id={userItem.id}
-            name={userItem.name}
-            email={userItem.email}
-            handleDeleteUser={handleDeleteUser}
-            isModalVisible={isModalVisible}
-            setIsModalVisible={setIsModalVisible}
-            handleEditUser={handleEditUser}
-          />
-        )
-      }
-    })
-  }
+  
+  
+ 
 
   return (
     <div className='home'>
-      <Modal title="Thêm người dùng"
-        width = '900px'
+      <Modal title="Edit role"
+        visible={isModalVisible}
+        onOk={() => { handleEditUser(isIdEdit) }}
+        onCancel={() => { setIsModalVisible(false) }}
+      >
+        <Radio.Group onChange={onChange} value={valueRadio}>
+          <Space direction="vertical">
+            <Radio value={'user'}>User</Radio>
+            <Radio value={'admin'}>Admin</Radio>
+            <Radio value={'banned'}>Ban</Radio>
+          </Space>
+        </Radio.Group>
+      </Modal>
+      <Modal title="Add User"
+        width="800px"
         visible={isModalVisible2}
         onOk={() => { setIsModalVisible2(false) }}
         onCancel={() => { setIsModalVisible2(false) }}
       >
         <Register />
       </Modal>
-      <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '14px' }}>
-        <span style={{ fontSize: '180%', }}>Quản lý người dùng</span>
-        <PlusSquareOutlined style={{ fontSize: '280%', color: '#1890ff', cursor: 'pointer' }} onClick={() => { setIsModalVisible2(true) }} />
+      <h2>Quản lý người dùng</h2>
+      <div style={{ display: 'flex',justifyContent : 'space-between', marginBottom: '14px' }}>
+        <div></div>
         <Search
-          style={{ width: 400 }}
           placeholder="Nhập vào thông tin"
           allowClear
           enterButton="Tìm kiếm"
           size="large"
+          style={{ width: 400 }}
           onSearch={onSearch}
         />
-
+        <div>
+          <PlusSquareOutlined
+            onClick={() => { setIsModalVisible2(true) }}
+            style={{ fontSize: '290%', color: '#008848', cursor: 'pointer' }}
+          />
+        </div>
       </div>
-      <table className='users' style={{ width: '100%' }}>
-        <tr>
-          <th>ID</th>
-          <th>User</th>
-          <th>Email</th>
-          <th>Trash</th>
-        </tr>
-        {renderUserList()}
-      </table>
+      <Table dataSource={userList.data}>
+        <Column title="ID" dataIndex="id" id="id" />
+        <Column title="Name" dataIndex="name" id="name" />
+        <Column title="Email" dataIndex="email" id="email" />
+        <Column title="Role" dataIndex="role" id="role" />
+        <Column
+          width='200px'
+          title="Action"
+          render={(record) => (
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <EditOutlined
+                onClick={() => { setIsModalVisible(true); setIsIdEdit(record.id) }}
+                style={{
+                  color: 'blue',
+                  cursor: 'pointer',
+                  fontSize: '180%'
+                }}
+              />
+              <UserDeleteOutlined
+                onClick={() => { handleDeleteUser(record.id) }}
+                style={{
+                  color: 'red',
+                  cursor: 'pointer',
+                  fontSize: '180%'
+                }}
+              />
+            </div>
+          )}
+        />
+      </Table>
     </div>
   );
 }
